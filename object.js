33c25540c59efd7c02bf754f0307beaadb0eab38
Jawad Hassan <itsjawadhasan@gmail.com>
2024-06-27 15:49:44 +0500

    Create object.js

---
+++ object.js
@@ -0,0 +1,14 @@
+const person = {
+firstname: Jawad,
+secondname: Hassan,
+age: 20,
+hobbies: ['swim','run','football'],
+address: {
+           street: '55th street',
+           city: 'islamabad',
+           country: 'pakistan',
+
+}
+}
+
+console.log(person)
