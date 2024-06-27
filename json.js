99a790a2ff044de8427bdb87e8f5b1900adbc3f5
Jawad Hassan <itsjawadhasan@gmail.com>
2024-06-27 17:03:34 +0500

    Create json.js

---
+++ json.js
@@ -0,0 +1,19 @@
+const todo= [
+{
+     id: 1, 
+    task1: 'do dishes',
+  isCompleted: true,
+},
+{
+    id: 2, 
+   task2: 'do cleaning',
+ isCompleted: true,
+},
+{
+    id: 3, 
+   task3: 'do washing',
+ isCompleted: true,
+}
+];
+const todojson= JSON.stringify(todo)
+console.log(todojson) // to send code to server
