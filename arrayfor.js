1d3f6c67f6fcbe0f5ff68b62c2a6ab9a332fdd72
Jawad Hassan <itsjawadhasan@gmail.com>
2024-06-27 17:13:33 +0500

    Create arrayfor.js

---
+++ arrayfor.js
@@ -0,0 +1,21 @@
+const todo= [
+{
+     id: 1, 
+    task: 'do dishes',
+  isCompleted: true,
+},
+{
+    id: 2, 
+   task: 'do cleaning',
+ isCompleted: true,
+},
+{
+    id: 3, 
+   task: 'do washing',
+ isCompleted: true,
+}
+];
+for(let i=0;i<todo.length;i++)
+    {
+    console.log(todo[i].task)
+    }
