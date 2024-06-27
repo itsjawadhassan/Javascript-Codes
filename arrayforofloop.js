0b0510b4e2fa62ad5d5bf6c09b5a7b28c5c5ffa3
Jawad Hassan <itsjawadhasan@gmail.com>
2024-06-27 17:16:51 +0500

    Create arrayforofloop.js

---
+++ arrayforofloop.js
@@ -0,0 +1,22 @@
+const todo= [
+{
+     id: 1, 
+    task: 'do dishes',
+  isCompleted: true,
+},
+{
+    id: 2, 
+   task: 'do cleaning',
+ isCompleted: true,
+},
+{
+    id: 3, 
+   task: 'do washing',
+ isCompleted: true,
+}
+];
+for(let i of todo)
+    {
+    console.log(i)
+    console.log(i.isCompleted)
+    }
