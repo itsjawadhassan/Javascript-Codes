dcae8d17f1f4f2cdca8b8a489ab968a1efb42f33
Jawad Hassan <itsjawadhasan@gmail.com>
2024-06-27 13:17:02 +0500

    Create tempstr.js

---
+++ tempstr.js
@@ -0,0 +1,7 @@
+const name= 'john'
+const age=30
+//concatenation
+console.log('my name is '+name+' and i am '+ age)
+//template strings
+const hello = `my name is ${name} and i am ${age}`
+console.log(hello)
