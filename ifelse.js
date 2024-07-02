406077c6a1f5af880d9cf05e67140ccc30837d58
Jawad Hassan <itsjawadhasan@gmail.com>
2024-07-02 14:59:18 +0500

    Create ifelse.js

---
+++ ifelse.js
@@ -0,0 +1,27 @@
+const todo= [
+{
+     id: 1, 
+    task: 'do dishes',
+  isCompleted: true,
+},
+{
+    id: 2, 
+   task: 'do cleaning',
+ isCompleted: true,
+},
+{
+    id: 3, 
+   task: 'do washing',
+ isCompleted: true,
+}
+];
+let x=10
+if(x>10)
+  {
+    console.log(todo[1].id)
+  
+  }
+  else
+  {
+    console.log(todo[2].id)
+  }
